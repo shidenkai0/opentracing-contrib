36b2d4c7d3a516c9e9e49356d0ee9da251491f04
Mohamed Messaad <mohamed.messaad@classnco.com>
2017-11-13 20:33:43 +0100

    Improve elastic request metadata, error handling, inject opentracing SpanContext in HTTP headers

--- github.com/olivere/elastic/elastic.go
+++ github.com/olivere/elastic/elastic.go
@@ -25,9 +25,10 @@ func (t *TracedTransport) RoundTrip(r *http.Request) (*http.Response, error) {
 	span, ctx := ot.StartSpanFromContext(r.Context(), "elastic.query")
 	r = r.WithContext(ctx)
 	defer span.Finish()
-	span.SetTag("elasticsearch.method", r.Method)
-	span.SetTag("elasticsearch.url", r.URL.Path)
-	span.SetTag("elasticsearch.params", r.URL.Query().Encode())
+
+	span.SetTag("elastic.method", r.Method)
+	span.SetTag("elastic.url", r.URL.Path)
+	span.SetTag("elastic.params", r.URL.Query().Encode())
 
 	contentLength, _ := strconv.Atoi(r.Header.Get("Content-Length"))
 	if r.Body != nil && contentLength < MaxContentLength {
@@ -35,16 +36,28 @@ func (t *TracedTransport) RoundTrip(r *http.Request) (*http.Response, error) {
 		if err != nil {
 			return nil, err
 		}
-		span.SetTag("elasticsearch.body", buf)
+		span.SetTag("elastic.body", string(buf))
 		r.Body = ioutil.NopCloser(bytes.NewBuffer(buf))
 	}
 
+	hhc := ot.HTTPHeadersCarrier(r.Header)
+	err := ot.GlobalTracer().Inject(span.Context(), ot.HTTPHeaders, hhc)
+	if err != nil {
+		return nil, err
+	}
+	span.SetTag("http.headers", r.Header)
 	// execute standard roundtrip
-	return t.Transport.RoundTrip(r)
+	resp, err := t.Transport.RoundTrip(r)
+	if err != nil {
+		span.SetTag("elastic.error", err.Error())
+		span.SetTag("error", true)
+	}
+	span.SetTag("elastic.status_code", resp.StatusCode)
+	return resp, err
 }
 
 // NewTracedHTTPClient returns a new http.Client with a custom transport dedicated to use with github.com/olivere/elastic
-func NewTracedHTTPClient(service string) *http.Client {
+func NewTracedHTTPClient() *http.Client {
 	return &http.Client{
 		Transport: &TracedTransport{&http.Transport{}},
 	}
