e9b397f13b8334e808d40e06ba7f91b95ee09681
Mohamed Messaad <mohamed.messaad@classnco.com>
2017-11-20 17:08:56 +0100

    use open-tracing-go/ext and set span kind for http server

--- net/http/server.go
+++ net/http/server.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	ot "github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
 	"github.com/urfave/negroni"
 )
 
@@ -13,16 +14,12 @@ func ServerMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// extract SpanContext from http headers carrier
 		sc, err := ot.GlobalTracer().Extract(ot.HTTPHeaders, ot.HTTPHeadersCarrier(r.Header))
-		// if we couldn't extract a SpanContext from the headers, proceed with serving the request using a new parent span
-		if err != nil {
-			span := ot.GlobalTracer().StartSpan(getOperationName(r.Method, r.URL.String()))
-			defer span.Finish()
-			ctx := ot.ContextWithSpan(r.Context(), span)
-			h.ServeHTTP(w, r.WithContext(ctx))
-			setHTTPTags(span, r, w)
-			return
+		var span ot.Span
+		if err == nil {
+			span = ot.GlobalTracer().StartSpan(getOperationName(r.Method, r.URL.String()), ot.ChildOf(sc))
+		} else { // if we couldn't extract a SpanContext from the headers, proceed with serving the request using a new parent span
+			span = ot.StartSpan(getOperationName(r.Method, r.URL.String()))
 		}
-		span := ot.GlobalTracer().StartSpan(getOperationName(r.Method, r.URL.String()), ot.ChildOf(sc))
 		defer span.Finish()
 		ctx := ot.ContextWithSpan(r.Context(), span)
 		// Use negroni.Responsewriter, providing facilities for getting response information
@@ -33,10 +30,12 @@ func ServerMiddleware(h http.Handler) http.Handler {
 }
 
 func setHTTPTags(span ot.Span, r *http.Request, w http.ResponseWriter) {
-	span.SetTag("http.method", r.Method)
-	span.SetTag("http.url", r.URL.String())
+	span.SetTag(string(ext.SpanKind), string(ext.SpanKindRPCServerEnum))
+	span.SetTag(string(ext.HTTPMethod), r.Method)
+	span.SetTag(string(ext.HTTPUrl), r.URL.Path)
+	span.SetTag(string(ext.PeerHostname), r.Host)
 	rw := w.(negroni.ResponseWriter)
-	span.SetTag("http.status_code", rw.Status())
+	span.SetTag(string(ext.HTTPStatusCode), rw.Status())
 	span.SetTag("http.response_body.size", rw.Size())
 }
 
