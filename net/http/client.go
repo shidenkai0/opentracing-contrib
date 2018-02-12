2294583f485c2693e0c08a66973b0f30ca08d7b2
Mohamed Messaad <mohamed.messaad@classnco.com>
2018-02-12 19:26:01 +0900

    mark 5xx status codes as error in http client instrumentation

--- net/http/client.go
+++ net/http/client.go
@@ -1,20 +1,12 @@
 package othttp
 
 import (
-	"bytes"
-	"io/ioutil"
 	"net/http"
-	"strconv"
 
 	"github.com/opentracing/opentracing-go/ext"
 
 	ot "github.com/opentracing/opentracing-go"
 )
-
-// MaxContentLength is the maximum content length for which we'll read and capture
-// the contents of the request body. Anything larger will still be traced but the
-// body will not be captured as trace metadata.
-const MaxContentLength = 1 << 16
 
 // TracedTransport is a traced HTTP transport that captures http call spans
 type TracedTransport struct {
@@ -36,16 +28,7 @@ func (t *TracedTransport) RoundTrip(r *http.Request) (resp *http.Response, err e
 
 	span.SetTag(string(ext.HTTPMethod), r.Method)
 	span.SetTag(string(ext.HTTPUrl), r.URL.String())
-
-	contentLength, _ := strconv.Atoi(r.Header.Get("Content-Length"))
-	if r.Body != nil && contentLength < MaxContentLength {
-		buf, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			return nil, err
-		}
-		span.SetTag(string(ext.DBStatement), string(buf))
-		r.Body = ioutil.NopCloser(bytes.NewBuffer(buf))
-	}
+	ext.SpanKindRPCClient.Set(span) // mark this span as a remote client call
 
 	err = ot.GlobalTracer().Inject(span.Context(), ot.HTTPHeaders, ot.HTTPHeadersCarrier(r.Header))
 	if err != nil {
@@ -58,6 +41,9 @@ func (t *TracedTransport) RoundTrip(r *http.Request) (resp *http.Response, err e
 		return nil, err
 	}
 	span.SetTag(string(ext.HTTPStatusCode), resp.StatusCode)
+	if resp.StatusCode/100 == 5 {
+		span.SetTag(string(ext.Error), true)
+	}
 	return resp, err
 }
 
